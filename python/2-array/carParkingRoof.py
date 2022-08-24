0ebc53fd77a7cdd0715d3771e68e975fd6887d08
Brian Ly <brianly1003@gmail.com>
2022-08-24 18:46:07 +0800

    Update Description for the carParkingRoof.py file

--- python/2-array/carParkingRoof.py
+++ python/2-array/carParkingRoof.py
@@ -1,3 +1,16 @@
+""" Amazon Logistics would like to quickly set up a roof over a parking lot. 
+There are many cars parked in the parking lot and the lot is in a straight line. There are n cars currently
+parked and a roofer needs to cover them with a roof . The requirement is that at least k cars currently in the lot are covered
+by the roof. Determine the minimum length of the roof to cover k cars.
+
+Example:
+  n = 4
+  cars = [6,2,12,7]
+  k = 3
+
+Two roofs that cover three cars are possible: one covering spots 2 through 7 with a length of 6, and another covering
+slots 6 through 12 with a length of 7. The shortest roof that meets the requirement is of length 6.
+"""
 
 from typing import List
 import math
